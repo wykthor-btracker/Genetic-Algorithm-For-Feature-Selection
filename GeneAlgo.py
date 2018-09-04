0cec8ef647c1952f4b0240ce5e7478e75c257e0c
Wykthor <wykthor.g@wgvale.com>
2018-09-04 17:10:44 -0300

    Error handling, more tests

--- GeneAlgo.py
+++ GeneAlgo.py
@@ -8,15 +8,36 @@
 #variables#
 
 #classes
+#Error handling
+class Error(Exception):
+    """Base Error Class"""
+    pass
+
+class GeneTypeError(TypeError):
+    def __init__(self,object):
+        self.object
+    def __str__(self):
+        return repr("{} is of type {}, {} required.".format(self.object,type(self.object),list))
+#Error handling#
 class geneAlgo:
     """
     This class will be used with a high-order function as the fitness measurement method,
-    using various genetic algorithm methods taken letter-by-letter from the literature.
+    working as the generation manipulation procedure, using various genetic algorithm methods
+    taken letter-by-letter from the literature(For Genetic Algorithms).
 
     The function passed for fitness measurement should return a float or intenger.
     """
     def __init__(self,fitnessFunction):
         self.fitnessFunction = fitnessFunction
+        return None
+
+class Individual:
+    """
+    This class will be used to handle single individual operations for each generation.
+    """
+    def __init__(self,gene):
+        if type(gene)!=list:
+            raise GeneTypeError(gene)
         return None
 #classes#
 
--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #imports
 import GeneAlgo
+from random import randint
 #imports#
 
 #variables
@@ -12,7 +13,7 @@ import GeneAlgo
 #classes#
 
 #functions
-def test_Class_Instance():
+def test_gene_Algo_Instance():
     newinstance = GeneAlgo.geneAlgo(lambda x:x*x)
     assert isinstance(newinstance,GeneAlgo.geneAlgo)
 
@@ -20,6 +21,14 @@ def test_Class_Fitness_Method():
     fitnessFunc = lambda x:x*x
     newinstance = GeneAlgo.geneAlgo(fitnessFunc)
     assert (type(newinstance.fitnessFunction(3)) == float or type(newinstance.fitnessFunction(3)) == int)
+
+def test_individual_Instance():
+    newinstance = GeneAlgo.Individual()
+    assert isinstance(newinstance,GeneAlgo.Individual)
+
+def test_Individual_Gene_Format():
+    gene = [randint(0,1) for val in range(10)]
+    newinstance = GeneAlgo.Individual(gene)
 #functions#
 
 #main
