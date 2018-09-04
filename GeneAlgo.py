65f1336ad24ffbf897451838c59f4617251ffc38
Wykthor <wykthor.g@wgvale.com>
2018-09-04 16:19:48 -0300

    More tests!

---
+++ GeneAlgo.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#imports
+
+#imports#
+
+#variables
+
+#variables#
+
+#classes
+class geneAlgo:
+    """
+    This class will be used with a high-order function as the fitness measurement method,
+    using various genetic algorithm methods taken letter-by-letter from the literature.
+
+    The function passed for fitness measurement should return a float or intenger.
+    """
+    def __init__(self,fitnessFunction):
+        self.fitnessFunction = fitnessFunction
+        return None
+#classes#
+
+#functions
+
+#functions#
+
+#main
+def main():
+    return None
+#main#
--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #imports
-
+import GeneAlgo
 #imports#
 
 #variables
@@ -12,8 +12,14 @@
 #classes#
 
 #functions
-def test_nothing():
-    assert True==True
+def test_Class_Instance():
+    newinstance = GeneAlgo.geneAlgo(lambda x:x*x)
+    assert isinstance(newinstance,GeneAlgo.geneAlgo)
+
+def test_Class_Fitness_Method():
+    fitnessFunc = lambda x:x*x
+    newinstance = GeneAlgo.geneAlgo(fitnessFunc)
+    assert (type(newinstance.fitnessFunction(3)) == float or type(newinstance.fitnessFunction(3)) == int)
 #functions#
 
 #main
